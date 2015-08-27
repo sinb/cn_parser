e5f8dceaa790673b0761db49779edf8c77cee914
sinb <linkin816@gmail.com>
2015-08-27 11:57:53 +0800

    trivial modifications

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,20 @@
 #include <sstream>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-
     locale::global(locale(""));
-    wcout.imbue(locale(""));
-    DictionaryParser dicparser;
-    vector<wstring> output;
-
-    while(!wcin.eof())
-    {
-      wstring input;
-      getline(wcin, input);
-      dicparser.parse(input, output);
-    }
-    for (int i = 0; i < output.size(); i++)
-        wcout << output[i] << " ";
-    wcout << endl;
-    
-//////////////////////////////////
-//     locale::global(locale(""));
-//     wcout.imbue(locale(""));  
-//     HMMParser a;
-//     a.getEmitProb();
+    wcout.imbue(locale(""));  
+      DictionaryParser dicparser;
+      vector<wstring> output;      
+      while(!wcin.eof())
+      {
+	wstring input;
+	getline(wcin, input);
+ 	dicparser.parse(input, output);
+      }
+      for (int i = 0; i < output.size(); i++)
+	  wcout << output[i] << " ";    
 
     return 0;
 }
